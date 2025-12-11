05ecd0575ec43a3ac605d7c7080ded1c10e2c55c
waleed.shahid.dev <waleed.shahid.dev@gmail.com>
2025-12-11 06:09:42 +0000

    changes to the app theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import events from '@/lib/constants'
 import React from 'react'
+import { cn } from "../lib/utils";
 
 
 
@@ -10,12 +11,12 @@ const Page = () => {
   return (
     <section>
         <h1 className='text-center'>The Hub for Every Dev <br/> Event You Can't Miss</h1>
-        <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place.</p>
+        <p className={cn('text-center', 'mt-5')}>Hackathons, Meetups, and Conferences, All in One Place.</p>
         <ExploreBtn/>
 
-        <div className='mt-20 space-y-7'>
+        <div className={cn('mt-20', 'space-y-7')}>
             <h3>Featured Events</h3>
-            <ul className='events list-none'>
+            <ul className={cn('events', 'list-none')}>
                 {
                     events.map((event,index)=>(
                         <li key={event.title}>
