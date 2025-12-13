ea4e3d92ffed9583e1913f37b2e92ac21cfbbbeb
waleed.shahid.dev <waleed.shahid.dev@gmail.com>
2025-12-13 15:52:37 +0000

    Setup Cloudinary for event image uploading

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import connectDB from "@/lib/mongodb"
 import { NextRequest, NextResponse } from "next/server"
 import Event from "@/database/event.model"
+import {v2 as cloudinary } from "cloudinary"
 export async function POST(req:NextRequest){
     try {
         
@@ -16,6 +17,34 @@ export async function POST(req:NextRequest){
 
         try {
             event = Object.fromEntries(formData.entries())
+
+            // get the image file blob
+
+            const file = formData.get("image") as File
+
+            if(!file) return NextResponse.json({message:"Image file is required"},{status:400})
+
+            // convert the blob to buffer 
+
+            const arrayBuffer = await file.arrayBuffer()
+
+            const buffer = Buffer.from(arrayBuffer)
+
+            // upload to the cloudnary 
+
+            const uploadResult = await new Promise((resolve, reject)=>{
+                cloudinary.uploader.upload_stream({
+                    resource_type:"image",
+                    folder:"DevEvent"
+            }, (error, results)=>{
+                if(error) return reject(error)
+                resolve(results)
+            }).end(buffer)
+            })    // returns a public url of the image 
+
+            event.image = (uploadResult as {secure_url:string}).secure_url
+
+ 
             const createEvent = await new Event(event)
 
             return NextResponse.json(
